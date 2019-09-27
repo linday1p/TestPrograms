0d079361e3f583daa8cfcfb6b6c2b2f39cca35d3
Erin Gunn <ikonone@gmail.com>
2019-09-27 10:47:36 -0600

    Added template class.

---
+++ src/edu/frcc/csclub/cc/TemplateCCApp.java
@@ -0,0 +1,40 @@
+package edu.frcc.csclub.cc;
+
+import java.awt.Color;
+
+public class TemplateCCApp extends CCApp {
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 500;
+
+	public TemplateCCApp() {
+		super("Template CCApp", WIDTH, HEIGHT);
+		// WARNING: Do not call getGfx() here.  It will break.
+		// All of your initialization code should go in the begin method.
+	}
+
+	@Override
+	public void begin() {
+		// TODO Initialize your application here.
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Update your application here.
+
+	}
+
+	@Override
+	public void paint() {
+		// TODO Draw your application here.
+		// e.g. 
+		getGfx().setColor(Color.blue);
+		getGfx().fillRect(0, 0, WIDTH, HEIGHT);
+	}
+
+	public static void main(String[] args) {
+		// TODO Start your application by creating it.
+		new TemplateCCApp();
+	}
+
+}
